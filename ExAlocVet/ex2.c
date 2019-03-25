cbab8c106f77e89a31280889fd6b45765e513ba1
daniel-libanori <48701920+daniel-libanori@users.noreply.github.com>
2019-03-25 11:30:49 -0300

    Adicionado Codigo exercicio 2 Alocação de Vetores

--- ExAlocVet/ex2.c
+++ ExAlocVet/ex2.c
@@ -1,1 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int negativos (int n, float *vet){
+    
+    acumula=0;
+
+    for(int i=0;i<n-1;i++)
+        if(vet[i]<0)
+            acumula++;
+
+    return acumula;
+
+
+
+}
+
+void lerVetorDin(int n, float *vet){
+
+    printf("\n");
+    for(int i=0;i<n-1;i++){
+        printf("n%d: ");
+        scanf("%f", &vet[i]);
+    }
+
+
+}
+
+
+void main(){
+
+    int n;
+    float *vetor;
+    int numNegativos=0;
+    
+
+    printf("Digite um tamanho n para o vetor: ");
+    scanf("%d", &n);
+    
+    lerVetorDin(n,vetor);
+    
+    numNegativos = negativos(n,vetor);
+
+    printf("Numero de Negativos: %d", numNegativos);
+
+
+
+
+}
